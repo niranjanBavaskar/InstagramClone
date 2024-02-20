7dfba957212b92a95ad216d45a022db2ab932d74
niranjanBavaskar <niranjanbavaskar301@gmail.com>
2024-02-20 11:19:45 +0530

    Updated - styles in login.module.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
-import {Login} from './pages/login/Login'
-//import { Registration } from "./pages/registration/Registration";
-
+import { Login } from "./pages/login/Login";
+import { Home } from "./pages/home/Home";
+import { Route, Routes } from "react-router-dom";
+import { Registration } from "./pages/registration/Registration";
 
 function App() {
-  return <div className="App">
-   <Login/>
-   
-  </div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Registration />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./login.module.css";
+import { FaSquareFacebook } from "react-icons/fa6";
 
 export const Login = () => {
   return (
@@ -7,12 +8,28 @@ export const Login = () => {
       <div className={styles.top}>
         <div className={styles.left}>Mobile gif</div>
         <div className={styles.right}>
-          Login box
-          <div className={styles.rightBottom}>
-          <div className={styles.facebookLogin}>Login with facebook</div>
-          <div className={styles.signUp}>Sign Up</div>
-          <div className={styles.getApp}>Get the app</div>
-        </div>
+          <div className={styles.signInBox}>
+            <div className={styles.appLogo}>
+              <img src=".\images\instagram.png" alt="" />
+            </div>
+
+            <div className={styles.loginForm}>
+              <input type="text" placeholder="phone no" />
+              <input type="text" placeholder="password" style={{marginTop:"6px"}}/>
+              <button>Login</button>
+            </div>
+            <div className={styles.divider}>
+              <div className={styles.line}></div>
+              <p>OR</p>
+            </div>
+            <div className={styles.socialLogin}>
+              <FaSquareFacebook />
+              <p>Login with Facebook</p>
+            </div>
+            <div className={styles.forgotPassword}>Forgot Password ?</div>
+          </div>
+          <div className={styles.signUpBox}>Sign up box</div>
+          <div className={styles.getAppBox}>Get the app</div>
         </div>
       </div>
       <div className={styles.bottom}>Meta info</div>
