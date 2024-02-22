34e42956a3eefff70ef1c530bf7c5c3ea1262975
niranjanBavaskar <niranjanbavaskar301@gmail.com>
2024-02-22 15:09:38 +0530

    Updated - styles in login page

--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -6,35 +6,53 @@ export const Registration = () => {
   return (
     <div className={styles.main}>
       <div className={styles.signUpFormContainer}>
-        <div className={styles.signUpForm}>
+        <div className={styles.top}>
           <div className={styles.appLogo}>
             <img src=".\images\instagram.png" alt="" />
           </div>
-          <p>Sign up to see photos and videos from your friends.</p>
+          <p>
+            Sign up to see photos and videos <br />
+            form your friends.
+          </p>
 
-          <button>
-            <FaSquareFacebook />
-            Login with facebook
-          </button>
+          <div className={styles.socialLogin}>
+            <button>
+              <FaSquareFacebook />
+              Log in with Facebook
+            </button>
+          </div>
           <div className={styles.divider}>
             <div className={styles.line1}></div>
             <div className={styles.or}>OR</div>
             <div className={styles.line2}></div>
           </div>
+        </div>
+        <div className={styles.mid}>
           <div className={styles.loginForm}>
-            <input type="text" placeholder="Mobile number or email" />
-            <input type="text" placeholder="Fullname" />
+            <input type="text" placeholder="Mobile Number or Email" />
+            <input type="text" placeholder="Full Name" />
             <input type="text" placeholder="Username" />
             <input type="text" placeholder="Password" />
-            <button>Sign up</button>
-            <p>
-              People who use our service may have uploded your contact
-              information to instagram.Learn more
-            </p>
-          <p>By signing up, you agree to our terms, Privacy Policy  and Cookies Policy.</p>
           </div>
         </div>
-        <div className={styles.signInCTA}>Log in</div>
+        <div className={styles.bottom}>
+          <p>
+            people who use our service may have uploaded
+            <br />
+            your contact information to instagram. Learn <br /> More
+          </p>
+          <p>
+            By signing up, you agree to our Terms, Privacy Policy <br /> and
+            Cookies Policy . 
+          </p>
+
+          <button>Sign up</button>
+        </div>
+      </div>
+
+      <div className={styles.signInCTA}>
+        <p>Have an account?</p>&nbsp;
+        <div className={styles.logIn}>Log in</div>
       </div>
     </div>
   );
